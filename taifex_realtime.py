f8e5334d36834f48c44190146547ba5ad55f63e6
sGoiTW <mliu.tw@gmail.com>
2019-08-17 03:29:24 +0800

    refact: open the file once in the beginning and unpack list instead of assign element one by one

--- taifex_realtime.py
+++ taifex_realtime.py
@@ -1,12 +1,17 @@
 import requests
 import datetime,time
 import csv
+import sys
 from bs4 import BeautifulSoup
 
 url = "http://info512ah.taifex.com.tw/Future/FusaQuote_Norl.aspx"
 
-goods = "臺指期108"
+contract = sys.argv[1]
+goods = "臺指期" + contract 
 date = datetime.datetime.now().strftime("%Y%m%d")
+
+row_format = "%-8s %-10s %8s %6s %6s %10s %10s"
+cvs_title = ["商品代號","時間","成交價","成交量","總量","最高價","最低價"]
 
 def get_my_hope_data(goods_name=goods):
     data = requests.get(url)
@@ -22,48 +27,33 @@ def split_data(data_selected):
     return origin_data
 
 def operate_data(data):
-    buy_price = data[0]
-    buy_amout = data[1]
-    sell_price = data[2]
-    sell_amout = data[3]
-    trade_price = data[4]
-    up_down = data[5]
-    Amplitude = data[6]
-    trade_amout = data[7]
-    star_price = data[8]
-    max_price = data[9]
-    min_price = data[10]
-    ref_price = data[11]
-    time = data[-1]
+    buy_price, buy_amout, sell_price, sell_amout, trade_price, up_down, Amplitude, trade_amout, star_price, max_price, min_price, ref_price, time = data
     return [goods,time,trade_price,buy_amout,trade_amout,max_price,min_price]
 
-def data_write(fileName="./%s/%s_Match.csv"%(goods,date),):
-    output_file = open (fileName,'a',newline='')
+def data_write(output_file):
     writer = csv.writer(output_file)
-    writer.writerow(["商品代號","時間","成交價","成交量","總量","最高價","最低價"])
-    output_file.close()
+    writer.writerow(cvs_title)
     record_time = 0
+    last_row = []
     yield
     while 1:
         write_data = yield
-        if record_time == write_data[1]:
+        if last_row != write_data:
+            print(row_format%(*write_data,))
+            last_row = write_data.copy()
             record_time = write_data[1]
-        else:
-            output_file = open(fileName, 'a', newline='')
             writer = csv.writer(output_file)
             writer.writerow(write_data)
-            record_time = write_data[1]
-            output_file.close()
 
 if __name__ == '__main__':
-    count = 0
-    write = data_write()
-    next(write)
-    while 1:
-        origindata = get_my_hope_data()
-        o_data = split_data(origindata)
-        print(o_data)
-        sort_data = operate_data(o_data)
-        write.send(sort_data)
-        time.sleep(5)
+    fileName = "./%s/%s_Match.csv"%(contract,date)
+    with open (fileName,'a',newline='') as output_file:
+        write = data_write(output_file)
+        next(write)
+        while 1:
+            origindata = get_my_hope_data()
+            o_data = split_data(origindata)
+            sort_data = operate_data(o_data)
+            write.send(sort_data)
+            time.sleep(1)
 
